1d8706525c355327e4e7a33be368cde1c4c53524
shobuj <shobujsa93@gmail.com>
2021-04-02 11:12:38 +0600

    set mutators & accessor for user model

--- app/User.php
+++ app/User.php
@@ -47,6 +47,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime', 'verification_token',
     ];
 
+
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = strtolower($name);   
+    }
+
+    public function getNameAttribute($name)
+    {
+        return ucwords($name);
+    }
+
+    public function setEmailAttribute($email)
+    {
+        $this->attributes['email'] = strtolower($email);
+    }
+
     public function isVerified(){
         return $this->verified == User::VERIFIED_USER;
     }
