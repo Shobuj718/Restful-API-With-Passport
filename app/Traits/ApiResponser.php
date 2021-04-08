5356a55dd482a929afdf5576353341a478960ade
shobuj <shobujsa93@gmail.com>
2021-04-08 08:59:16 +0600

    caching system and the query parameters

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -34,6 +35,8 @@ trait ApiResponser
 		$collection = $this->paginate($collection);
 
 		$collection = $this->transformData($collection, $transformer);
+
+		$collection = $this->cacheResponse($collection);
 
 		return $this->successResponse($collection, $code);
 	}
@@ -110,5 +113,21 @@ trait ApiResponser
 		$transformation = fractal($data, new $transformer);
 
 		return $transformation->toArray();
+	}
+
+	public function cacheResponse($data)
+	{
+		$url = request()->url();
+		$queryParams = request()->query();
+
+		ksort($queryParams);
+
+		$queryString = http_build_query($queryParams);
+
+		$fullUrl = "{$url}?{$queryString}";
+
+		return Cache::remember($fullUrl, 30, function() use($data) {
+			return $data;
+		});
 	}
 }
