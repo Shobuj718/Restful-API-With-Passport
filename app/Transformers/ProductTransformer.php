7f81a56063e793dc7790898db7f993e2f9e371df
shobuj <shobujsa93@gmail.com>
2021-04-08 10:17:34 +0600

    implement HATEOAS Hypermedia controls

--- app/Transformers/ProductTransformer.php
+++ app/Transformers/ProductTransformer.php
@@ -43,6 +43,35 @@ class ProductTransformer extends TransformerAbstract
             'creationDate' => $product->created_at,
             'lastChanged' => $product->updated_at,
             'deletedDate' => isset($product->deleted_at) ? (string) $product->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('products.show', $product->id),
+                ],
+
+                [
+                    'rel' => 'product.buyers',
+                    'href' => route('products.buyers.index', $product->id),
+                ],
+
+                [
+                    'rel' => 'product.categories',
+                    'href' => route('products.categories.index', $product->id),
+                ],
+
+                [
+                    'rel' => 'product.transactions',
+                    'href' => route('products.transactions.index', $product->id),
+                ],
+
+                [
+                    'rel' => 'seller',
+                    'href' => route('sellers.show', $product->id),
+                ],
+
+            ],
+
         ];
     }
 
